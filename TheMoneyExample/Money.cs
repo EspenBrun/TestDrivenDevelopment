5038a8b3462b591098ce896c98ad18882bed31fa
Espen Kirkesæther Brun <ekb@patientsky.com>
2019-07-13 09:14:55 +0200

    Ch 8: Makin' objects

--- TheMoneyExample/Money.cs
+++ TheMoneyExample/Money.cs
@@ -6,6 +6,16 @@ namespace TheMoneyExample
     {
         protected int Amount;
 
+        public static Money Dollar(int amount)
+        {
+            return new Dollar(amount);
+        }
+
+        public static Money Franc(int amount)
+        {
+            return new Franc(amount);
+        }
+
         public override bool Equals(object obj)
         {
             Money other;
@@ -20,5 +30,7 @@ namespace TheMoneyExample
             }
             return Amount == other.Amount && obj.GetType() == GetType();
         }
+
+        public abstract Money Times(int i);
     }
 }
--- TheMoneyExampleTests/DollarTests.cs
+++ TheMoneyExampleTests/DollarTests.cs
@@ -8,41 +8,41 @@ namespace TheMoneyExampleTests
         [Fact]
         public void Multiplication()
         {
-            var five = new Dollar(5);
+            var five = Money.Dollar(5);
 
             var fifteen = five.Times(3);
 
-            Assert.Equal(new Dollar(5), five);
-            Assert.Equal(new Dollar(15), fifteen);
+            Assert.Equal(Money.Dollar(5), five);
+            Assert.Equal(Money.Dollar(15), fifteen);
         }
 
         [Fact]
         public void Multiplication_Franc()
         {
-            var five = new Franc(5);
+            var five = Money.Franc(5);
 
             var fifteen = five.Times(3);
 
-            Assert.Equal(new Franc(5), five);
-            Assert.Equal(new Franc(15), fifteen);
+            Assert.Equal(Money.Franc(5), five);
+            Assert.Equal(Money.Franc(15), fifteen);
         }
 
         [Fact]
         public void Equals_SameAmount_AreEqual()
         {
-            Assert.True(new Dollar(5).Equals(new Dollar(5 )));
+            Assert.True(Money.Dollar(5).Equals(Money.Dollar(5 )));
         }
 
         [Fact]
         public void Equals_DifferentAmount_AreNotEqual()
         {
-            Assert.False(new Dollar(5).Equals(new Dollar(6)));
+            Assert.False(Money.Dollar(5).Equals(Money.Dollar(6)));
         }
 
         [Fact]
         public void Equals_DollarAndFranc_NotEqual()
         {
-            Assert.False(new Dollar(5).Equals(new Franc(5)));
+            Assert.False(Money.Dollar(5).Equals(Money.Franc(5)));
         }
     }
 }
