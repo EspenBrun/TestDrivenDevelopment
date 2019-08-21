33841bd6e5f3f43daa54bfec152af59449c5856c
Espen Kirkesæther Brun <ekb@patientsky.com>
2019-08-22 00:05:50 +0200

    Ch 18: First Steps to xUnit: Dynamically invoke method from constructor argument

--- xunit-tdd.py
+++ xunit-tdd.py
@@ -1,15 +1,25 @@
 class WasRun:
+	'''This is the docstring'''
 	def __init__(self, name):
 		self.wasRun = None
 		self.name = name
 	def testMethod(self):
-		print("The wasRun object was called. The constructor argument for name was: " + self.name)
+		print("The testMethod was called. The constructor argument for name was: " + self.name)
 		self.wasRun = 1
 	def run(self):
-		print("The interface to testMethod(), the method run(), was executed")
-		self.testMethod()
+		print("The interface to a method in this class was called. I will fail unless you use one of these:")
+		# self.testMethod()
+		# print(self.__doc__)
+		print(dir(self))
+		method = getattr(self, self.name)
+		print("got method with name " + self.name)
+		method()
+	def methodA(self):
+		print("method A called")
+	def methodB(self):
+		print("methodB called")
 
-test = WasRun("someName")
+test = WasRun("methodB")
 print(test.wasRun)
 # test.testMethod()
 # dont want to run the test method directly, use an interface
