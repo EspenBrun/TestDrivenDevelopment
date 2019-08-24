15d0f72f3c3d02e35d770a323d46524e4edffa93
Espen Kirkesæther Brun <ekb@patientsky.com>
2019-08-24 22:25:07 +0200

    Ch 18: First Steps to xUnit: Comment out what is now done in the TestCaseTest

--- xunit-tdd.py
+++ xunit-tdd.py
@@ -36,18 +36,3 @@ class TestCaseTest(TestCase):
 		assert(test.wasRun)
 
 TestCaseTest("testRunning").run()
-
-test = WasRun("testMethod")
-print("### Test start")
-print("")
-print("Checking that test is not run yet. test.wasRun should be None:")
-print("test.wasRun is: ")
-print(test.wasRun)
-print("")
-print("Will now run the testMethod through the run() proxy")
-# dont want to run the test method directly, use an proxy
-test.run()
-
-print("Checking that test has run. test.wasRun should be 1:")
-print("test.wasRun is:")
-print(test.wasRun)
